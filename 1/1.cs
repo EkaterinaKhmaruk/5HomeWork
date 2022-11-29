a5cb14e54cec43558d935ab26a444ba1aa7e2ce3
Ekaterina Khmaruk <ekaterinabond43@gmail.com>
2022-11-29 16:20:22 +0500

    Created falders

---
+++ 1/1.cs
@@ -0,0 +1,19 @@
+﻿/* Задайте массив заполненный случайными положительными трёхзначными числами.
+Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2*/
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+int count = 0;
+
+Console.Write("Массива рандом: ");
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    Console.Write(array[i] + " ");
+    if (array[i] % 2 == 0) count++;
+}
+Console.WriteLine();
+Console.WriteLine($"Количество четных элементов: {count}");
---
+++ 3/3.cs
@@ -0,0 +1,39 @@
+﻿/* Задайте массив вещественных чисел. Найдите разницу между 
+максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76*/
+
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[size];
+double diffrent = 0;
+
+Random rand = new Random();
+
+
+Console.Write("Массива рандом: ");
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.NextDouble();
+    Console.Write(array[i] + " ");
+}
+
+double min = array[0];
+double max = array[0];
+
+for (int i = 0; i < array.Length; i++)
+{
+    
+    if (array[i] > max ) max = array[i];
+    if (array[i] < min ) min = array[i];
+}
+
+
+diffrent = max - min;
+Console.WriteLine();
+Console.WriteLine("Минимальное число: " + min);
+Console.WriteLine("Максимальное число: " + max);
+Console.WriteLine($"Разница между max и min {diffrent}");
+            
